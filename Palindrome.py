b3212249f54b338f2fffe399fdbeb9eb60535822
Gangadhar27-S <gani165@gmail.com>
2025-04-09 22:18:15 +0530

    Update Palindrome.py

--- Palindrome.py
+++ Palindrome.py
@@ -2,15 +2,15 @@ s = "malayalam"  # string
 
 i,j = 0, len(s) - 1  # two pointers
 
-is_palindrome = True  # assume palindrome
+is_Palindrome = True  # assume palindrome
 while i < j:
     if s[i] != s[j]:  # mismatch found
-        is_palindrome = False
+        is_Palindrome = False
         break
     i += 1
     j -= 1
 
-if is_palindrome:
+if is_Palindrome:
     print("Yes") 
 else:
     print("No")  
